d69a472abb0c71a66305b5b686e53a606e8be141
rachitak <57540964+rachitak@users.noreply.github.com>
2020-05-31 15:39:52 -0700

    initial commit

---
+++ src/main/java/mavenTestProject/mavenTestProject/App.java
@@ -0,0 +1,14 @@
+package mavenTestProject.mavenTestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "oh why hello there!" );
+    }
+}
